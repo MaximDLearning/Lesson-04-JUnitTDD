b7a0bbfd6277db2177edd66678084a677f7794bb
Ubuntu <maxim.dalmeida@outlook.com>
2021-02-14 16:35:20 +0000

    Integrated with JUnit5

--- TestSrc/com/tddJunit/java/MathsTest.java
+++ TestSrc/com/tddJunit/java/MathsTest.java
@@ -7,30 +7,30 @@ import org.junit.Test;
 
 public class MathsTest {
 
-	//Comment-1: private Maths classundertest;	
+	private Maths classundertest;	
 
 	@Before
 	public void setUp() throws Exception {
 
-		//comment-2: classundertest = new Maths();
+		classundertest = new Maths();
 
 	}
 
 	@Test
 	public void testAdd() {
-		//comment-3 : fail("Not yet implemented");
-
-		/* comment-4:	long result = 10+2;
-assertEquals(result, classundertest.add(10,2)); */
+		
+		long result = 10+2;
+		assertEquals(result, classundertest.add(10,2));
 
 	}
 
 	@Test
 	public void testSubtract() {
-	//comment-5: fail("Not yet implemented");
-	/* comment-6: long result = 10-2;
-		assertEquals(result, classundertest.subtract(10,2));*/	
+	
+		long result = 10-2;
+		assertEquals(result, classundertest.subtract(10,2));
 
 	}
 
 }
+
